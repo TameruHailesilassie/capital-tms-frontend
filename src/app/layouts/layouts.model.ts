636380d2cc0b84f1f2e03e8b6b68e51d77fb29f8
Tame <tameruh.s@gmail.com>
2022-03-31 21:59:20 +0300

    🐛 fix(UI): Layout width fixed

--- src/app/layouts/layouts.model.ts
+++ src/app/layouts/layouts.model.ts
@@ -3,7 +3,7 @@ export const LAYOUT_VERTICAL = 'vertical';
 
 export const LAYOUT_HORIZONTAL = 'horizontal';
 
-export const LAYOUT_WIDTH = 'boxed';
+export const LAYOUT_WIDTH = 'fluid';
 
 export const SIDEBAR_TYPE = 'light';
 
