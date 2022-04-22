baf71e867a26d4eb2c47d08d86823e256e8b8b46
Tame <tameruh.s@gmail.com>
2022-04-22 13:54:46 +0300

    🎉 feat(dashboard): Map added for Personal dashboard

--- src/app/modules/analytics/personalanalytics/personalanalytics.component.ts
+++ src/app/modules/analytics/personalanalytics/personalanalytics.component.ts
@@ -19,6 +19,8 @@ export class PersonalanalyticsComponent implements OnInit {
   reportStartDate: string;
   reportEndDate: string;
   loadSummaryChart: PieChartModel;
+  pickUpCoverage: any[];
+  deliveryCoverage: any[];
   ranges: any = {
     "Current Month": [moment().startOf("month"), moment().endOf("month")],
     "Previous Month": [
@@ -63,6 +65,216 @@ export class PersonalanalyticsComponent implements OnInit {
     });
     this.loadSummaryChart = pieChart;
     this.UpdateReportData();
+    this.pickUpCoverage = [
+      {
+        name: "Alabama",
+        value: 200,
+      },
+      {
+        name: "Alaska",
+        value: 600,
+      },
+      {
+        name: "Arizona",
+        value: 10,
+      },
+      {
+        name: "Arkansas",
+        value: 5,
+      },
+      {
+        name: "California",
+        value: 2,
+      },
+      {
+        name: "Colorado",
+        value: 2000,
+      },
+      {
+        name: "Connecticut",
+        value: 0,
+      },
+      {
+        name: "Delaware",
+        value: 1,
+      },
+      {
+        name: "District of Columbia",
+        value: 0,
+      },
+      {
+        name: "Florida",
+        value: 0,
+      },
+      {
+        name: "Georgia",
+        value: 0,
+      },
+      {
+        name: "Hawaii",
+        value: 0,
+      },
+      {
+        name: "Idaho",
+        value: 0,
+      },
+      {
+        name: "Illinois",
+        value: 1500,
+      },
+      {
+        name: "Indiana",
+        value: 100,
+      },
+      {
+        name: "Iowa",
+        value: 120,
+      },
+      {
+        name: "Kansas",
+        value: 16,
+      },
+      {
+        name: "Kentucky",
+        value: 5,
+      },
+      {
+        name: "Louisiana",
+        value: 2,
+      },
+      {
+        name: "Maine",
+        value: 0,
+      },
+      {
+        name: "Maryland",
+        value: 0,
+      },
+      {
+        name: "Massachusetts",
+        value: 0,
+      },
+      {
+        name: "Michigan",
+        value: 0,
+      },
+      {
+        name: "Minnesota",
+        value: 0,
+      },
+      {
+        name: "Mississippi",
+        value: 58,
+      },
+      {
+        name: "Missouri",
+        value: 1,
+      },
+      {
+        name: "Montana",
+        value: 0,
+      },
+      {
+        name: "Nebraska",
+        value: 63,
+      },
+      {
+        name: "Nevada",
+        value: 32,
+      },
+      {
+        name: "New Hampshire",
+        value: 50,
+      },
+      {
+        name: "New Jersey",
+        value: 1,
+      },
+      {
+        name: "New Mexico",
+        value: 1,
+      },
+      {
+        name: "New York",
+        value: 1900,
+      },
+      {
+        name: "North Carolina",
+        value: 8,
+      },
+      {
+        name: "North Dakota",
+        value: 0,
+      },
+      {
+        name: "Ohio",
+        value: 1200,
+      },
+      {
+        name: "Oklahoma",
+        value: 166,
+      },
+      {
+        name: "Oregon",
+        value: 98,
+      },
+      {
+        name: "Pennsylvania",
+        value: 2,
+      },
+      {
+        name: "Rhode Island",
+        value: 0,
+      },
+      {
+        name: "South Carolina",
+        value: 0,
+      },
+      {
+        name: "South Dakota",
+        value: 0,
+      },
+      {
+        name: "Tennessee",
+        value: 0,
+      },
+      {
+        name: "Texas",
+        value: 0,
+      },
+      {
+        name: "Utah",
+        value: 0,
+      },
+      {
+        name: "Vermont",
+        value: 0,
+      },
+      {
+        name: "Virginia",
+        value: 1,
+      },
+      {
+        name: "Washington",
+        value: 52,
+      },
+      {
+        name: "West Virginia",
+        value: 22,
+      },
+      {
+        name: "Wisconsin",
+        value: 2,
+      },
+      {
+        name: "Wyoming",
+        value: 66,
+      },
+      {
+        name: "Puerto Rico",
+        value: 0,
+      },
+    ];
   }
   private UpdateReportData() {
     console.log(
