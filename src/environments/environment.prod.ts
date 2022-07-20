8b67e2ea448449f57d12fd347b237c5c349b2c25
Tame <tameruh.s@gmail.com>
2022-07-20 18:31:17 +0300

    🔧 refactor(carrier): clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  saferWatchBaseURL:"https://www.saferwatch.com/webservices/CarrierService32.php?Action=",
   baseURL:"",
   defaultauth: 'fackbackend',
   firebaseConfig: {
